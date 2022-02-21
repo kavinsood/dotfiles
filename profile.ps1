edc88e309b3180773da47a6ac339b10a1caccd48
k2s09 <kavin@kavinsood.com>
2022-02-21 19:45:56 +0530

    added powershell profile

---
+++ profile.ps1
@@ -0,0 +1,11 @@
+# Autocomplete
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# Aliases
+New-Alias open ii
+New-Alias l ls
+
+# New Prompt
+Invoke-Expression (&starship init powershell)
