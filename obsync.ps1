2497c2733059f6572775dd0b7ee528dd6e326ecb
Kavin Sood <moneykavinsood@gmail.com>
2022-07-10 21:47:28 +0530

    updated sync

--- obsync.ps1
+++ obsync.ps1
@@ -1,16 +1,15 @@
-# Syncs the obsidian folder in onedrive with github as a backup platform
+#!/bin/bash
+# Sync the obsidian folder with github as a backup and sync platform
 # Add the folder containing this file to PATH for easy access
 
 # Move to git repo
-cd ~/github/obsidian
+cd ~/github/Obsidian
 
-# First delete all present in the git repo
+# First all files from onedrive into git repo
 rm *
 
-# Move all files from onedrive into git repo
-cp ~/OneDrive/Obsidian/*.* .
+# Move all files from local folder into git repo
+cp ~/OneDrive/Obsidian/* .
 
-# Add, commit and push local changes to branch
-git add . 
-git commit -m "$(Get-date)"
-git push
+# Run gitsync script to add, commit and push local changes to branch
+gitsync.ps1
