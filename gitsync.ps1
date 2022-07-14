54b8ea0a9b2a559051c797eac6bfa022d7f1f6b5
Kavin Sood <moneykavinsood@gmail.com>
2022-07-14 22:35:54 +0530

    07/14/2022 22:35:54

--- gitsync.ps1
+++ gitsync.ps1
@@ -3,23 +3,23 @@
 
 # First check if the current directory is a git repository
 if (Test-Path .git -PathType Any) {
-    # Always commit before introducing external changes
-    if("$(git status --porcelain)") {
-        git add . 
-        git commit -m "$(Get-Date)"
-        # Stash your local changes
-        git stash
-        # Update branch to latest code from remote server
-        git pull
-        # Merge your local changes into the latest code
-        git stash apply
-        # Add, commit and push local changes to branch
-        git add . 
-        git commit -m "$(Get-date)"
-        git push
-    } else {
-    	Write-Output "No changes found"
-    }
+# Always commit before introducing external changes
+	if("$(git status --porcelain)") {
+		git add . 
+			git commit -m "$(Get-Date)"
+# Stash your local changes
+			git stash
+# Update branch to latest code from remote server
+			git pull
+# Merge your local changes into the latest code
+			git stash apply
+# Add, commit and push local changes to branch
+			git add . 
+			git commit -m "$(Get-date)"
+			git push
+	} else {
+		Write-Output "No changes found"
+	}
 } else {
-    Write-Output "Git repository not initialised at current path"
+	Write-Output "Git repository not initialised at current path"
 }
