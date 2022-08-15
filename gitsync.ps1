d73709dd329324b21f34a077c6fff6d9fa65f00b
Kavin Sood <68739182+k2s09@users.noreply.github.com>
2022-08-15 21:35:11 +0530

    Cleaned

--- gitsync.ps1
+++ gitsync.ps1
@@ -1,9 +1,10 @@
 # Syncs a github repository two way in a simple script to run with one line
 # Add the folder containing this file to PATH for easy access
-
 # First check if the current directory is a git repository
+
 if (Test-Path .git -PathType Any) {
 # Always commit before introducing external changes
+
 	if("$(git status --porcelain)") {
 		git add . 
 			git commit -m "$(Get-Date)"
@@ -20,6 +21,8 @@ if (Test-Path .git -PathType Any) {
 	} else {
 		Write-Output "No changes found"
 	}
+	
 } else {
 	Write-Output "Git repository not initialised at current path"
+	
 }
