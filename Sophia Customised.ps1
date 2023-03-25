a9d01c00aec5ddd2eb85f5a185ce292e6cc3e8f9
Kavin Sood <68739182+k2s09@users.noreply.github.com>
2023-03-25 13:53:14 +0530

    little thing

---
+++ Sophia Customised.ps1
@@ -0,0 +1,213 @@
+﻿#Requires -RunAsAdministrator
+#Requires -Version 5.1
+
+[CmdletBinding()]
+param
+(
+	[Parameter(Mandatory = $false)]
+	[string[]]
+	$Functions
+)
+
+Clear-Host
+
+$Host.UI.RawUI.WindowTitle = "Sophia Script for Windows 11 v6.2.7 | Made with $([char]::ConvertFromUtf32(0x1F497)) of Windows | $([char]0x00A9) farag & Inestic, 2014$([char]0x2013)2023"
+
+Remove-Module -Name Sophia -Force -ErrorAction Ignore
+Import-Module -Name $PSScriptRoot\Manifest\Sophia.psd1 -PassThru -Force
+
+Import-LocalizedData -BindingVariable Global:Localization -BaseDirectory $PSScriptRoot\Localizations -FileName Sophia
+
+<#
+	.SYNOPSIS
+	Run the script by specifying functions as an argument
+	Запустить скрипт, указав в качестве аргумента функции
+
+	.EXAMPLE
+	.\Sophia.ps1 -Functions "DiagTrackService -Disable", "DiagnosticDataLevel -Minimal", UninstallUWPApps
+
+	.NOTES
+	Use commas to separate funtions
+	Разделяйте функции запятыми
+#>
+if ($Functions)
+{
+	Invoke-Command -ScriptBlock {Checks}
+
+	foreach ($Function in $Functions)
+	{
+		Invoke-Expression -Command $Function
+	}
+
+	# The "RefreshEnvironment" and "Errors" functions will be executed at the end
+	Invoke-Command -ScriptBlock {RefreshEnvironment; Errors}
+
+	exit
+}
+
+
+#region Protection
+
+Checks -Warning
+
+#endregion Protection
+
+#region Privacy & Telemetry
+
+DiagTrackService -Disable
+DiagnosticDataLevel -Minimal
+ErrorReporting -Disable
+FeedbackFrequency -Never
+ScheduledTasks -Disable
+SigninInfo -Enable
+LanguageListAccess -Disable
+AdvertisingID -Disable
+SettingsSuggestedContent -Hide
+AppsSilentInstalling -Disable
+WhatsNewInWindows -Disable
+TailoredExperiences -Disable
+BingSearch -Disable
+
+#endregion Privacy & Telemetry
+
+#region UI & Personalization
+
+ThisPC -Show
+CheckBoxes -Disable
+HiddenItems -Enable
+FileExtensions -Show
+MergeConflicts -Show
+OpenFileExplorerTo -QuickAccess
+OneDriveFileExplorerAd -Show
+FileTransferDialog -Detailed
+QuickAccessRecentFiles -Show
+QuickAccessFrequentFolders -Show
+TaskbarAlignment -Center
+TaskbarSearch -Hide
+TaskViewButton -Hide
+TaskbarWidgets -Hide
+TaskbarChat -Hide
+ControlPanelView -Category
+WindowsColorMode -Dark
+AppColorMode -Dark
+FirstLogonAnimation -Enable
+JPEGWallpapersQuality -Max
+RestartNotification -Show
+ShortcutsSuffix -Disable
+PrtScnSnippingTool -Enable
+AeroShaking -Enable
+Cursors -Default
+
+#endregion UI & Personalization
+
+#region OneDrive
+
+
+#endregion OneDrive
+
+#region System
+
+StorageSense -Enable
+StorageSenseFrequency -Month
+StorageSenseTempFiles -Enable
+Hibernation -Disable
+Win32LongPathLimit -Disable
+BSoDStopError -Enable
+DeliveryOptimization -Disable
+WindowsManageDefaultPrinter -Disable
+WindowsFeatures -Disable
+WindowsCapabilities -Uninstall
+UpdateMicrosoftProducts -Enable
+PowerPlan -Balanced
+WinPrtScrFolder -Default
+RecommendedTroubleshooting -Automatically
+ReservedStorage -Disable
+F1HelpPage -Disable
+NumLock -Enable
+StickyShift -Disable
+Autoplay -Disable
+ThumbnailCacheRemoval -Disable
+SaveRestartableApps -Disable
+NetworkDiscovery -Enable
+ActiveHours -Automatically
+DefaultTerminalApp -WindowsTerminal
+
+#endregion System
+
+#region WSL
+
+
+#endregion WSL
+
+#region Start menu
+
+RunPowerShellShortcut -NonElevated
+
+#endregion Start menu
+
+#region UWP apps
+
+HEIF -Install
+CortanaAutostart -Disable
+TeamsAutostart -Disable
+UninstallUWPApps
+CheckUWPAppsUpdates
+
+#endregion UWP apps
+
+#region Gaming
+
+XboxGameBar -Enable
+XboxGameTips -Disable
+GPUScheduling -Enable
+
+#endregion Gaming
+
+#region Scheduled tasks
+
+
+#endregion Scheduled tasks
+
+#region Microsoft Defender & Security
+
+NetworkProtection -Enable
+PUAppsDetection -Enable
+AuditProcess -Disable
+CommandLineProcessAudit -Disable
+EventViewerCustomView -Disable
+PowerShellModulesLogging -Disable
+PowerShellScriptsLogging -Disable
+AppsSmartScreen -Enable
+SaveZoneInformation -Disable
+WindowsScriptHost -Disable
+WindowsSandbox -Enable
+
+#endregion Microsoft Defender & Security
+
+#region Context menu
+
+MSIExtractContext -Show
+CABInstallContext -Show
+RunAsDifferentUserContext -Hide
+CastToDeviceContext -Show
+ShareContext -Hide
+EditWithClipchampContext -Hide
+PrintCMDContext -Hide
+IncludeInLibraryContext -Hide
+SendToContext -Hide
+CompressedFolderNewContext -Hide
+MultipleInvokeContext -Enable
+UseStoreOpenWith -Hide
+OpenWindowsTerminalContext -Show
+OpenWindowsTerminalAdminContext -Disable
+Windows10ContextMenu -Enable
+
+#endregion Context menu
+
+#region Update Policies
+
+
+#endregion Update Policies
+
+RefreshEnvironment
+Errors
