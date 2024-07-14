e86a7a40a6d3a628cf21172ab1d1916d2427f8fc
Kavin Sood <68739182+k2s09@users.noreply.github.com>
2024-07-15 02:42:16 +0530

    vscode and term changes

--- profile.ps1
+++ profile.ps1
@@ -4,10 +4,14 @@ Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 # Aliases
-New-Alias l ls
+New-Alias ls eza -Force
+New-Alias l eza
 New-Alias open ii
 function googleSearch{Start-Process www.google.com/search?q=$args}
 Set-Alias g googleSearch
+function perplexitySearch{Start-Process www.perplexity.ai/search?q=$args}
+Set-Alias p perplexitySearch
 
 # Starship Prompt
 Invoke-Expression (&starship init powershell)
+
